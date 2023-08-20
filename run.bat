b3f720030c7b5035ac4705722d38f445326aca4c
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-08-20 08:57:14 -0400

    Initial commit - run.bat

---
+++ run.bat
@@ -0,0 +1,11 @@
+Sure! Here is an example of the run.bat file contents:
+
+```
+@echo off
+pip install -r requirements.txt
+python main.py
+```
+
+Save the above code in a file named "run.bat" in the same directory as your main.py and requirements.txt files. 
+
+To run the program, just double-click on the run.bat file. It will automatically install the required dependencies using pip and then execute the main.py file.
